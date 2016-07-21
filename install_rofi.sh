522e961e0d47c8b067b7f732a387a9003e582b4c
John M Daly <jmdaly@gmail.com>
2016-07-20 21:35:57 -0400

    Update to latest version of rofi

--- install_rofi.sh
+++ install_rofi.sh
@@ -6,7 +6,7 @@
 
 # First, install the build dependencies
 apt-get install -y git make gcc checkinstall autoconf automake pkg-config
-apt-get install -y libxinerama-dev libxft-dev libpango1.0-dev libcairo2-dev libpangocairo-1.0-0 libglib2.0-dev libx11-dev libstartup-notification0-dev 
+apt-get install -y libxinerama-dev libxft-dev libpango1.0-dev libcairo2-dev libpangocairo-1.0-0 libglib2.0-dev libx11-dev libstartup-notification0-dev libxcb-ewmh-dev libxcb-ewmh2
 
 INSTALL_PREFIX=/usr/local
 
@@ -14,11 +14,12 @@ INSTALL_PREFIX=/usr/local
 if [ ! -d ${INSTALL_PREFIX}/src/rofi ]; then
   mkdir -p ${INSTALL_PREFIX}/src
   cd ${INSTALL_PREFIX}/src
-  git clone https://github.com/DaveDavenport/rofi.git 
+  git clone https://github.com/DaveDavenport/rofi.git
 fi
 
-ROFI_VERSION=0.15.12
+ROFI_VERSION=1.1.0
 cd ${INSTALL_PREFIX}/src/rofi
+git submodule update --init
 git fetch --tags
 git checkout ${ROFI_VERSION}
 autoreconf -i
