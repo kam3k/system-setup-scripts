40986a513d9629cd697764dd8b1abfa39af1700d
John M Daly <jmdaly@gmail.com>
2017-04-27 10:37:22 -0400

    Update: tmux to latest version

--- install_tmux.sh
+++ install_tmux.sh
@@ -11,7 +11,7 @@ apt-get install -y automake make gcc libevent-dev libncurses5-dev
 apt-get install -y xsel
 
 INSTALL_PREFIX=/usr/local
-TMUX_VERSION=2.3
+TMUX_VERSION=2.4
 
 # Get the source:
 if [ ! -d ${INSTALL_PREFIX}/src/tmux ]; then
